a644f440e6400a0385c425f5f5eef23446fe3350
Plat <github@p1at.dev>
2022-09-25 18:38:21 +0900

    fix: add some exports

--- lib/monster_siren.dart
+++ lib/monster_siren.dart
@@ -4,5 +4,7 @@
 library monster_siren;
 
 export "src/monster_siren_client.dart";
-
-// TODO: Export any libraries intended for clients of this package.
+export "src/service/album/album_detail.dart";
+export "src/service/albums/albums_item.dart";
+export "src/service/song/song_detail.dart";
+export "src/service/songs/songs_item.dart";
